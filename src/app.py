b54098e8c9dc82855fc40372486c1112fdc41b57
Serge Kondratov <37714042+Serunya@users.noreply.github.com>
2023-10-21 00:49:43 +0300

    работа с переменными докера

--- src/app.py
+++ src/app.py
@@ -1,1 +1,13 @@
-print("xui")
+import os
+import sys
+def start_bot(token):
+    sys.stderr.write("token:" + token)
+    print(token)
+
+
+token = os.environ.get('tg_token')
+
+if token:
+    start_bot(token)
+else:
+    sys.stderr.write("token not found")
