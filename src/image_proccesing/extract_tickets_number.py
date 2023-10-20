7f5cf8fb18adb63c47be905e11c1caff17860faa
Serge Kondratov <37714042+Serunya@users.noreply.github.com>
2023-10-20 21:27:58 +0300

    тест распознования изображений

--- src/image_proccesing/extract_tickets_number.py
+++ src/image_proccesing/extract_tickets_number.py
@@ -7,12 +7,9 @@ def main():
     test_image_processing()
 
 def test_image_processing():
-    image = cv2.imread('img.png')
+    image = PIL.Image.open('img2.jpg')
     text = pytesseract.image_to_string(image)
     print(text)
-
-
-
 
 
 main()
