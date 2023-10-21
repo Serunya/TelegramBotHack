c0e1727b7aa14c02077b3d59e0e7414890231e18
Serge Kondratov <37714042+Serunya@users.noreply.github.com>
2023-10-21 10:26:27 +0300

    test

--- src/commands/Hello.py
+++ src/commands/Hello.py
@@ -6,4 +6,4 @@ from aiogram.types import Message
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message):
-    await message.answer(f"Hello, {message.from_user.full_name}!")
+    await message.answer(f"Ne hello, {message.from_user.full_name}!")
