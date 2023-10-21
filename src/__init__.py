24bf83983f90c748f4f017bdbaf8e0561adf4147
Serge Kondratov <37714042+Serunya@users.noreply.github.com>
2023-10-21 12:33:25 +0300

    dsa

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,18 @@
-from . import app
+import os
+import sys
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher
+import commands
+from aiogram.filters import CommandStart
+
+dp = Dispatcher()
+async def start_bot(token):
+    bot = Bot(token)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    token = "dsa"
+    asyncio.run(start_bot(token))
