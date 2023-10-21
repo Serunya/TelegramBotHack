0425e9af99625f76bed6fe3947ff8e5060809f4b
Serge Kondratov <37714042+Serunya@users.noreply.github.com>
2023-10-21 10:51:17 +0300

    fix reuirments

--- src/app.py
+++ src/app.py
@@ -14,5 +14,5 @@ async def start_bot(token):
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    token = os.environ.get('tg_token')
+    token = "dsa"
     asyncio.run(start_bot(token))
