caa7f41e409aa50d5e92c03f28c4b6fc89a271a7
Serge Kondratov <37714042+Serunya@users.noreply.github.com>
2023-10-21 10:23:53 +0300

    начало работы бота

--- src/app.py
+++ src/app.py
@@ -1,14 +1,11 @@
 import os
 import sys
-<<<<<<< HEAD
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
 
 dp = Dispatcher()
-
-
 async def start_bot(token):
     bot = Bot(token)
     await dp.start_polling(bot)
@@ -18,15 +15,3 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     token = os.environ.get('tg_token')
     asyncio.run(start_bot(token))
-=======
-def start_bot(token):
-    pass
-
-
-token = os.environ.get('tg_token')
-
-if token:
-    start_bot(token)
-else:
-    sys.stderr.write("token not found")
->>>>>>> main
